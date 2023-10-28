fbf0dfcf1d54f1c007f2822be89e2fce101ed3e1
Simpler1504 <artemlaptev2602@gmail.com>
2023-10-28 21:13:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create form
+        WelcomeWindow welcome = new WelcomeWindow();
+    }
+}
---
+++ src/WelcomeWindow.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class WelcomeWindow extends JFrame {
+    private JLabel label = new JLabel("Вітаємо вас у грі, під назвою \"Міста\"!");
+    private JButton button = new JButton("Почати");
+    private  JFrame jFrame = new JFrame("Вітаємо!");
+    public WelcomeWindow() {
+        Image image = Toolkit.getDefaultToolkit().getImage("D:\\IdeaProjects\\Game_Cities\\images\\icon.png");
+        jFrame.setBounds(764,448,400,100);
+        label.setBounds(20,20,250,30);
+        button.setBounds(250,20,100,30);
+        jFrame.setIconImage(image);
+        jFrame.setLayout(null);
+        jFrame.setVisible(true);
+        jFrame.add(button);
+        jFrame.add(label);
+
+        jFrame.setDefaultCloseOperation(3);
+
+        button.addActionListener(new WelcomeWindow.ButtonEventListener());
+    }
+
+    class ButtonEventListener implements ActionListener {
+        // Button click
+        public void actionPerformed(ActionEvent e){
+
+            jFrame.setVisible(false);
+        }
+    }
+}
